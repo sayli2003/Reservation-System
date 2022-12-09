ed9945cf4098a5dadcbb3142fe92836d0b5229c5
Manasi Deshmukh <115893690+ManasiGDeshmukh@users.noreply.github.com>
2022-12-09 20:36:23 +0530

    Update BUSMain.java

--- src/brs/BUSMain.java
+++ src/brs/BUSMain.java
@@ -1,37 +1,229 @@
-package brs;
-import java.util.Scanner;
-//here is the trial update
-//this is updated on github
-class Bus{
+package miniproj;
+import java.io.*;
+import java.util.*;
 
-	String name;
-	long ID;
-	String time;
-	int seats[][];
-	String stops[];
-	int Passengers;
-	PassengerDetails[] Psg;
-	Bus(long ID,int noofstops){
-		this.ID=ID;
-		Psg= new PassengerDetails[10];
-		seats=new int[noofstops][10];
-		stops=new String[noofstops];
+// main class
+public class BUSMain {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int g=1, ch=0;	// for menu
+		int[] depd; // ?
+
+		BusOperation bo = new BusOperation();	// operation class
+		bo.initiate(); // Initialize all buses
+
+		do {
+			System.out.println(" ----------------------------------------");
+			System.out.println("  1. BOOKING   2. CANCELLATION  3. EXIT");
+			ch=sc.nextInt();
+			System.out.println("");
+
+			switch(ch) {
+			case 1:
+				System.out.println("          Available Stops");
+				System.out.println("--------------------------------------");
+				for(int i=0; i< bo.AvailableStops.length; i++) {
+					System.out.print(" "+ bo.AvailableStops[i]+" | ");
+					if(i%4==3) {
+						System.out.println("");
+					}
+				}
+				System.out.println();
+				System.out.println("--------------------------------------");
+				System.out.print("  Enter the departure:  ");
+				String dep=sc.next();
+				System.out.print("  Enter the destination:  ");
+				String des=sc.next();
+				depd=bo.search(dep,des);
+
+
+				if(depd!=null) {
+					System.out.println("Enter Bus Id: ");
+					Bus bus=bo.BusFinder(sc.nextInt());
+					bo.bookSeat(bus, depd);
+				}
+				break;
+
+			case 2:
+				System.out.println();
+				break;
+
+			case 3:
+				g=0;
+				break;
+
+			default:
+				System.out.println("");
+			}
+		}while(g==1);
+		sc.close();
 	}
 }
 
-
-class PassengerDetails{									//Class BusBook
-	String PassengerName;
-	int noofseats,id,SeatNo,dep ,des;
-}
-
-
-
-class BusOperation{							//Class BusOperation
+class BusOperation{
 	Scanner sc = new Scanner(System.in);
-
 	int busNo,  n;
 	Bus[] TheArr=new Bus[5];
+	// Available Stops
+	String[] AvailableStops = {"Pune", "Nagar", "Jalgaon", "Hadapsar", "Pimpri",
+			"Karve", "Swargate", "Satara", "Sangli", "Solapur", "Beed", "Mumbai", 
+			"Raigarh", "Ratnagiri"};
+
+	int[] search(String dep, String des){		
+		int departureFound=0, found_flag=0;
+		int[] depd=new int[2];
+		//Iterating through all busses
+		for(int i=0;i<TheArr.length;i++) {
+			//Iterating thru stops
+			for(int j=0;j<TheArr[i].stops.length;j++) {
+				//checking for departure
+				if(departureFound==1) {
+
+					if(des.equalsIgnoreCase(TheArr[i].stops[j])) {
+						found_flag=1;
+						depd[1]=j;
+						System.out.println(" -------------------------------");
+						System.out.println("  Bus ID: "+TheArr[i].ID);
+						System.out.println("  Bus Name: "+TheArr[i].name);
+						System.out.println("  Bus Time: "+TheArr[i].time);
+						//	DisplayBusDetails(TheArr[i]);
+						System.out.println(" -------------------------------");
+						break;
+					}
+				}
+				else if(dep.equalsIgnoreCase(TheArr[i].stops[j])) {
+					departureFound=1;
+					depd[0]=j;
+				}				
+			}
+			departureFound=0;
+
+		}if(found_flag==0) {
+			System.out.println("\n  Bus not available !!!!\n");
+			return null;
+		}
+		return depd;
+	}
+
+	Bus BusFinder(int BusId) {
+		for (int i=0;i<TheArr.length;i++) {
+			if(TheArr[i].ID==BusId) {
+				return TheArr[i];
+			}
+		}
+		return null;
+	}
+
+	void Show_Seats(Bus bus, PassengerDetails psg) {
+		System.out.println("    ------------");
+		System.out.print("   | ");
+		for(int j=0; j<bus.seats[0].length/2;j++) {
+			if(CheckStatus_print(bus,psg,j)) {
+				System.out.print((j+1)+" ");
+			}
+			else {
+				System.out.print("B ");
+			}
+		}
+		System.out.println(" |");
+		System.out.print("   | ");
+		for(int j=bus.seats[0].length/2; j<bus.seats[0].length;j++) {
+			if(CheckStatus_print(bus,psg,j)) {
+				System.out.print((j+1)+" ");
+			}
+			else {
+				System.out.print("B ");
+			}
+		}
+		System.out.print("|  --> DRIVER\n");
+		System.out.println("    ------------");
+	}
+	
+	void bookSeat(Bus bus,int[] depd) {		
+		//Accepting passenger details		
+		PassengerDetails psg=new PassengerDetails();
+		System.out.print("  Enter number of seats: ");
+		int seats=sc.nextInt();
+		sc.nextLine();
+		//		DisplaySeats();
+		//		if(SeatCounter(bus,seats)) {
+		for(int a=0;a<seats;a++) {
+			System.out.print("  Enter your name: ");
+			psg.PassengerName=sc.nextLine();
+			Show_Seats(bus,psg);
+			System.out.println();
+			System.out.print("  Enter seat number: ");
+			psg.SeatNo=sc.nextInt();
+			sc.nextLine();
+			psg.dep=depd[0];
+			psg.des=depd[1];
+			System.out.println("  Do you want to confirm booking?? Y/N");
+			String chr=sc.nextLine();
+			char ch= chr.charAt(0);
+			if(ch=='y' || ch=='Y') {
+				System.out.println();
+				CheckStatus(bus,psg);
+			}
+		}
+		//		}
+		//		else {
+		//			System.out.println("  Seats not avalible !!!");
+		//		}
+
+
+	}
+	
+	void ChangeStatus(Bus bus, PassengerDetails psg) {
+		for(int m=psg.dep;m<=psg.des;m++) {
+			bus.seats[m][psg.SeatNo - 1]=1;
+		}
+
+	}
+
+	void CheckStatus(Bus bus,PassengerDetails psg) {			//Seat booking
+		int seatflag=0;
+		System.out.println("before");
+		for(int i=0; i<bus.stops.length; i++) {
+			for(int j=0; j<bus.seats[i].length;j++) {
+				System.out.print(bus.seats[i][j]+" ");
+			}System.out.println();
+		}
+		for(int m=psg.dep;m<=psg.des;m++) {
+			if(bus.seats[m][psg.SeatNo - 1]==0) {
+				bus.seats[m][psg.SeatNo - 1]=1;
+				seatflag=1;
+			}
+			else if (seatflag!=1) {
+				System.out.println("Seat already booked.Please choose another seat.");
+				System.out.println("Enter seat number: ");
+				psg.SeatNo=sc.nextInt();
+				CheckStatus(bus,psg);
+				break;
+			}
+		}
+		System.out.println("after");
+		for(int i=0; i<bus.stops.length; i++) {
+			for(int j=0; j<bus.seats[i].length;j++) {
+				System.out.print(bus.seats[i][j]+" ");
+			}System.out.println();
+		}
+	}
+
+	void Save_psg_data(PassengerDetails psg) {
+		
+	}
+	
+	Boolean CheckStatus_print(Bus bus,PassengerDetails psg, int x) {			//Seat booking
+//		int seatflag=0;
+		for(int m=psg.dep;m<=psg.des;m++) {
+			if ( bus.seats[m][x]==1) {
+				return false;
+			}
+		}
+
+		return true;
+
+	}
 
 	void initiate() {
 
@@ -92,217 +284,30 @@ class BusOperation{							//Class BusOperation
 		TheArr[4].stops[2]="Ratnagiri";
 		TheArr[4].seats[2]=new int[]{0,0,0,0,0,0,0,0,0,0};
 	}
-	void DisplayAllBuses() {
-		for(int i=0;i<5;i++) {
-			System.out.println("Bus No: "+(i+1));
-			System.out.println("The Bus Name: "+TheArr[i].name);
-			for(int j=0;j<TheArr[i].stops.length;j++) {
-				System.out.println("Stops: "+TheArr[i].stops[j]);
-			}
-			System.out.println("_______________________________________");
-		}
-	}
 
-
-	int[] search(String dep,String des) {						//Searches for the available buses
-		int departureFound=0, found_flag=0;
-		int m=0, id=0;
-		int[] depd=new int[2];
-		//Iterating through all busses
-		for(int i=0;i<TheArr.length;i++) {
-			//Iterating tjru stops
-			for(int j=0;j<TheArr[i].stops.length;j++) {
-				//checking for departure
-				if(departureFound==1) {
-
-					if(des.equalsIgnoreCase(TheArr[i].stops[j])) {
-						found_flag=1;
-						depd[1]=j;
-						System.out.println("-------------------------------");
-						System.out.println("Bus ID: "+TheArr[i].ID);
-						System.out.println("Bus Name: "+TheArr[i].name);
-						System.out.println("Bus Time: "+TheArr[i].time);
-						//						DisplayBusDetails(TheArr[i]);
-						System.out.println("-------------------------------");
-						break;
-					}
-				}
-				else if(dep.equalsIgnoreCase(TheArr[i].stops[j])) {
-					departureFound=1;
-					depd[0]=j;
-					
-				}
-				
-			}
-
-		}if(found_flag==0) {
-			System.out.println("Bus not available");
-			return null;
-		}
-		return depd;
-
-
-	}
-	//	void DisplayBusDetails(Bus bus) {
-	//		System.out.println("-------------------------------");
-	//		System.out.println("Bus ID: "+bus.ID);
-	//		System.out.println("Bus Name: "+bus.name);
-	//		System.out.println("Bus Time: "+bus.time);
-	//		
-	//	}
-
-	void bookSeat(Bus bus,int[] depd) {		
-		//Accepting passenger details		
-		PassengerDetails psg=new PassengerDetails();
-		System.out.println("Enter number of seats:");
-		int seats=sc.nextInt();
-		DisplaySeats();
-		if(SeatCounter(bus,seats)) {
-			for(int a=0;a<seats;a++) {
-				System.out.println("Enter your name:");
-				psg.PassengerName=sc.nextLine();
-				System.out.println("Enter seat number: ");
-				psg.SeatNo=sc.nextInt();
-				psg.dep=depd[0];
-				psg.des=depd[1];
-				System.out.println("Do you want to confirm booking?? Y/N");
-				char ch=sc.nextLine().charAt(0);
-				if(ch=='y' || ch=='Y') {
-					ChangeStatus(bus,psg);
-				}
-			}
-		}
-		else {
-			System.out.println("Seats not avalible");
-		}
-
-
-	}
-		void ChangeStatus(Bus bus,PassengerDetails psg) {			//Seat booking
-			int seatflag=0;
-			for(int m=psg.dep;m<psg.dep;m++) {
-				if(bus.seats[m][psg.SeatNo]==0) {
-					bus.seats[m][psg.SeatNo]=1;
-					seatflag=1;
-				}
-				else {
-					System.out.println("Seat already booked.Please choose another seat.");
-					System.out.println("Enter seat number: ");
-					psg.SeatNo=sc.nextInt();
-					ChangeStatus(bus,psg);
-	//				break;
-				}
-		}
-		}
-		boolean SeatCounter(Bus bus,int seats) {				//Counting empty seats
-			int trueforall=1;
-			int counter=0;
-			System.out.println("Inside Seat Counter");
-			for(int j=0;j<bus.seats[0].length;j++) {
-				for(int k=0;k<bus.stops.length;k++) {
-				if(bus.seats[k][j]!=0) {
-					trueforall=0;
-				}
-				}
-				if(trueforall==1) {
-					counter++;
-				}
-			}
-			if(counter>=seats) {
-				return true;
-			}
-			return false;
-		}
-	//	void display(int noofseats, int id) {
-	//		for(int i=0; i<1; i++) {
-	//			System.out.println("Bus Name: "+TheArr[i].name);
-	//			System.out.println("Passenger ID: "+ id);
-	//			System.out.println("Passenger name: "+bus[i].PassengerName);
-	//			System.out.println("Depature: "+bus[i].dp);
-	//			System.out.println("Destination: "+bus[i].d);
-	//			System.out.println("Bus type: "+bus[i].bustype);
-	//			System.out.println("No of seats: "+bus[i].noofseats);
-	//
-	//		}
-	//
-	//	}
-	//	void waiting() {
-	//		int g=0, MaxSize, rear, front;
-	//		System.out.println("Do you want to be in waiting list.");
-	//		g = sc.nextInt();
-	//		if(g==1) {
-	//
-	//		}
-	//
-	//	}
-	Bus BusFinder(int BusId) {
-		int BusfoundFlg=0;
-		int cancelseat;
-		for (int i=0;i<TheArr.length;i++) {
-			if(TheArr[i].ID==BusId) {
-				return TheArr[i];
-			}
-		}
-		return null;
-	}
-	//	
-	//	void cancellation() {
-	//		String cancelname;
-	//		int BusId,BusfoundFlg=0;
-	//		int cancelseat;
-	//		System.out.println("Enter Bus Id");
-	//		BusId=sc.nextInt();
-	//		for (int i=0;i<TheArr.length;i++) {
-	//			if(TheArr[i].ID==BusId) {
-	//				BusfoundFlg=1;
-	//				break;
-	//			}
-	//		}
-	//		if(BusfoundFlg==0) {
-	//			System.out.println("Bus NOT found");
-	//		}
-	//		
-	//		System.out.print("Enter your name: ");
-	//		cancelname = sc.next();
-	//		System.out.print("Enter your Seat Number: ");
-	//		cancelseat = sc.nextInt();
-	//		
-	//	}
-	//}
-	// testing clone repo
 }
 
 
-public class BUSMain{
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int g = 0, ch = 0;
-		int[] depd;
-		BusOperation bo = new BusOperation();
-		bo.initiate();
-		do {
-		System.out.println("Enter the departure:");
-		String dep=sc.next();
-		System.out.println("Enter the destination:");
-		String des=sc.next();
-		depd=bo.search(dep,des);
-		if(depd!=null) {
-			System.out.println("1.Booking\n2.Cancellation.");
-			ch= sc.nextInt();
-			switch(ch){
-			case 1: 
-				System.out.println("Enter Bus Id: ");
-				Bus bus=bo.BusFinder(sc.nextInt());
-				
-						break;
-				//			case 2: bo.cancellation();
-				//			break;
-			}
-		}
-			
-			System.out.println("Do you want to continue? if yes press 1 else press 0");
-			g=sc.nextInt();
-		}while(g==1);
+
+
+class Bus{		// storing arr class for arr obj
+
+	String name, time;
+	long ID;
+	int seats[][];
+	String stops[];
+	int Passengers;
+	PassengerDetails[] Psg;
+	Bus(long ID,int noofstops){
+		this.ID=ID;
+		Psg= new PassengerDetails[10];
+		seats=new int[noofstops][10];
+		stops=new String[noofstops];
 	}
+}
+
+
+class PassengerDetails{									//Class BusBook
+	String PassengerName;
+	int noofseats,id,SeatNo,dep ,des;
 }
